dd9174dc3a4f3c0475f2d3cccad6be7eecca1a1b
GMottaGit <guilhermega1946@gmail.com>
2024-12-11 10:52:55 -0300

    botão de salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,42 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
-    Connection conn;
+
+    conectaDAO db = new conectaDAO();
+    Connection conn = db.connectDB();
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        try {
+            String sql = "INSERT INTO produtos(nome, valor, status) VALUES" + "(?, ?, ?)";
+            PreparedStatement smts = this.conn.prepareStatement(sql);
+            smts.setString(1, produto.getNome());
+            smts.setInt(2, produto.getValor());
+            smts.setString(3, produto.getStatus());
+            smts.execute();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso.");
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao cadastrar produto: " + e.getMessage());
+
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,30 +4,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+
+    public Connection connectDB() {
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11", "root", "12345678");;
+            return conn;
+        } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            return null;
         }
-        return conn;
+        
     }
-    
+
+    public void desconectar(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+
+            }
+        } catch (SQLException ex) {
+            System.out.println("Nao foi possivel desconectar do banco dados.");
+        }
+    }
 }
